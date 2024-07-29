cc5b3039edfeee331b465c91fab63a4375b76aa3
findlaywebb <findlayawebb@gmail.com>
2024-07-30 00:06:47 +0100

    Refactor model declaration and training pipeline
    Extract the generalised training pipeline to a separate module for better maintainability and to
    ensure consistency between models.
    
    This will allow for easier development and hyperparamater tuning.
    
    bert.py has been refactor to demonstrate this new method.
    These changes could break other pre-existing models.

--- models/bert.py
+++ models/bert.py
@@ -1,49 +1,37 @@
-import time
-import argparse
-
 from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
 import tensorflow as tf
 import numpy as np
 
-from model_preprocessing import get_data
-from prediction_processing import post_process, save_meta_data
+from model_pipeline import run_model_pipeline, fetch_data, parse_args
 
 from sklearn.utils.class_weight import compute_class_weight
 
 MODEL_NAME = 'BERT'
 
 
+from typing import Dict, Any
+import numpy as np
+import tensorflow as tf
+from sklearn.utils.class_weight import compute_class_weight
+from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
 
-timings = {}
+def declare_model(model_vars: Dict[str, Any]) -> None:
+    """
+    Declare and configure the model architecture.
+    We will use the DistilBERT model for sequence classification.
+    We are using Adam optimizer with an initial learning rate of 2e-5.
 
-if __name__ == '__main__':
+    Args:
+        model_vars (Dict[str, Any]): A dictionary containing the required variables for the model.
 
-    timings['start'] = time.perf_counter()
+    Returns:
+        None
+    """
 
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Naive Bayes model')
-    parser.add_argument('--generate_substrings', type=str, default='none', const='random', nargs='?', help='Generate random substrings')
-    parser.add_argument('--random_substrings', type=int, default=10, help='Number of random substrings to generate')
-    args = parser.parse_args()
-
-    # Append model name with 'random' and number of random substrings 
-    # if generating random substrings
-    if args.generate_substrings == 'random':
-        MODEL_NAME += f'__random_{args.random_substrings}'
-
-    print(f'Model name: {MODEL_NAME}')
-
-    # Get the data
-    timings['get_data:start'] = time.perf_counter()
-    X_train, X_test, y_train, y_test, le = get_data(
-        generate_substrings=args.generate_substrings,
-        random_substrings=args.random_substrings
-        )
-    timings['get_data:end'] = time.perf_counter()
-
-    print(f'Data preparation time: {timings["get_data:end"] - timings["get_data:start"]:0.4f}')
-
-    timings['model_declaration:start'] = time.perf_counter()
+    # Declare variables from model_vars
+    X_train = model_vars['X_train']
+    X_test = model_vars['X_test']
+    y_train = model_vars['y_train']
 
     # Load pre-trained BERT tokenizer and model
     # Using distilbert-base-uncased model as full BERT is too large
@@ -51,34 +39,19 @@ if __name__ == '__main__':
     tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
     model = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=2)
 
-
     # Padding and tokenisation
+    # Truncation is used to ensure the input length is less than the maximum
+    # input length of the model
+    # For DistilBERT, the maximum input length is 512
     X_train_enc = tokenizer(list(X_train), padding=True, truncation=True, return_tensors='tf')
     X_test_enc = tokenizer(list(X_test), padding=True, truncation=True, return_tensors='tf')
 
-    # # For code testing just take 1/20 of the data
-    # X_train_enc = {key: value[:len(value)//20] for key, value in X_train_enc.items()}
-    # y_train = y_train[:len(y_train)//20]
-
-    timings['model_declaration:end'] = time.perf_counter()
-
-    # Fit the model
-    timings['fit:start'] = time.perf_counter()
-
-    # Compute class weights
+    # Compute class weights - our True label only represents ~33% of the data
     class_weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
     class_weights = {i: class_weights[i] for i in range(len(class_weights))}
-    # class_weights = tf.constant(list(class_weights.values()))
-
 
     # Set learning rate
     optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
-
-    model.compile(
-        optimizer=optimizer,
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=['accuracy']
-    )
 
     # Callback for learning rate scheduling and early stopping
     callbacks = [
@@ -86,23 +59,59 @@ if __name__ == '__main__':
         tf.keras.callbacks.LearningRateScheduler(lambda epoch: 2e-5 if epoch < 2 else 1e-5, verbose=1)
     ]
 
+    # Save variables to model_vars
+    model_vars['model'] = model
+    model_vars['X_train_enc'] = X_train_enc
+    model_vars['X_test_enc'] = X_test_enc
+    model_vars['class_weights'] = class_weights
+    model_vars['optimizer'] = optimizer
+    model_vars['callbacks'] = callbacks
+
+
+def train_model(model_vars: Dict[str, Any]) -> None:
+    """
+    Train the model.
+    BERT works well with larger batch sizes and fewer epochs.
+    Epochs are set to 4 and batch size is set to 12 (limited by GPU memory).
+
+    Args:
+        model_vars (Dict[str, Any]): A dictionary containing the required variables for the model.
+    """
+
+    # Declare variables from model_vars
+    model = model_vars['model']
+
+    model.compile(
+        optimizer=model_vars['optimizer'],
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
     model.fit(
-        X_train_enc,
-        y_train,
+        model_vars['X_train_enc'],
+        model_vars['y_train'],
         epochs=4,
         batch_size=12,
         validation_split=0.2,
-        class_weight=class_weights,
-        callbacks=callbacks
+        class_weight=model_vars['class_weights'],
+        callbacks=model_vars['callbacks']
     )
 
-    timings['fit:end'] = time.perf_counter()
+def predict(model_vars: Dict[str, Any]) -> None:
+    """
+    Perform predictions using the trained model.
 
-    print(f'Training time: {timings["fit:end"] - timings["fit:start"]:0.4f}')
+    BERT outputs logits, which are converted to probabilities using softmax.
+    The probabilities are then converted to binary predictions using a threshold of 0.5.
 
-    # Evaluate the model
-    timings['predict:start'] = time.perf_counter()
-    y_pred_nb_proba = model.predict(X_test_enc)
+    Args:
+        model_vars (Dict[str, Any]): A dictionary containing the required variables for the model.
+    """
+
+    # Declare variables from model_vars
+    model = model_vars['model']
+
+    y_pred_nb_proba = model.predict(model_vars['X_test_enc'])
 
     # Convery logits to probabilities
     y_pred_nb_proba = tf.nn.softmax(y_pred_nb_proba.logits, axis=-1).numpy()  # Convert logits to probabilities
@@ -112,18 +121,18 @@ if __name__ == '__main__':
 
     print(y_pred_nb_proba)
     # y_pred_nb_proba = y_pred_nb_proba.flatten()
-
-    timings['predict:end'] = time.perf_counter()
-    print(f'Prediction time: {timings["predict:end"] - timings["predict:start"]:0.4f}')
     y_pred_nb = np.where(y_pred_nb_proba > 0.5, 1, 0)
+    
+    # Save variables to model_vars
+    model_vars['y_pred_nb_proba'] = y_pred_nb_proba
+    model_vars['y_pred_nb'] = y_pred_nb
 
-    timings['end'] = time.perf_counter()
 
-    post_process(X_test, y_test, y_pred_nb, y_pred_nb_proba, le, model_name=MODEL_NAME)
+if __name__ == '__main__':
 
-    data_size = {
-        'train': len(X_train),
-        'test': len(X_test)
-    }
+    model_vars = {}
 
-    save_meta_data(timings, data_size, model_name=MODEL_NAME)
+    # Parse command line arguments
+    args, MODEL_NAME = parse_args(MODEL_NAME)
+
+    run_model_pipeline(MODEL_NAME, fetch_data, declare_model, train_model, predict, model_vars, args)
